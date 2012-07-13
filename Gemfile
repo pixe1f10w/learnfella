02fbed39ce5b2c617b8a5a3d7a8eb6bfcf2a0cae
Ilia Zhirov <izhirov@gmail.com>
2012-07-13 13:58:02 +0600

    complete exercises

--- Gemfile
+++ Gemfile
@@ -5,10 +5,10 @@ gem 'rails', '3.2.6'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development, :test do
-  gem 'sqlite3', '1.3.5'
-  gem 'rspec-rails', '2.10.0'
-end
+#group :development, :test do
+#  gem 'sqlite3', '1.3.5'
+#  gem 'rspec-rails', '2.10.0'
+#end
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -23,14 +23,24 @@ group :assets do
 end
 
 gem 'jquery-rails'
+gem 'pg', '0.12.2'
+
+group :development, :test do
+  gem 'rspec-rails', '2.10.0'
+  gem 'guard-rspec', '0.5.5'
+end
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
-group :production do
-  gem 'pg', '0.12.2'
-end
+#group :production do
+#  gem 'pg', '0.12.2'
+#end
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe 'Static pages' do
+  let( :base_title ) { "Ruby on Rails Tutorial Sample App" }
+
   describe 'Home page' do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -10,7 +12,7 @@ describe 'Static pages' do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector 'title', :text => "Ruby on Rails Tutorial Sample App | Home"
+      page.should have_selector 'title', :text => "#{base_title} | Home"
     end
   end
 
@@ -23,7 +25,7 @@ describe 'Static pages' do
 
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector 'title', :text => "Ruby on Rails Tutorial Sample App | Help"
+      page.should have_selector 'title', :text => "#{base_title} | Help"
     end
   end
 
@@ -36,7 +38,19 @@ describe 'Static pages' do
 
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector 'title', :text => "Ruby on Rails Tutorial Sample App | About Us"
+      page.should have_selector 'title', :text => "#{base_title} | About Us"
+    end
+  end
+
+  describe 'Contact page' do
+    it "should have the content 'Contact information'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'h1', :text => 'Contact information'
+    end
+
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'title', :text => "#{base_title} | Contact"
     end
   end
 end
